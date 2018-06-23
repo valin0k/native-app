a8f1774cccb07054e965b56a931cbdd85a36cb0e
Valin0k <serpork@ya.ru>
2018-06-23 23:00:52 +0700

    add simple config

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React, {Fragment} from 'react'
+import {render} from 'react-dom'
+
+const Index = () => (
+  <Fragment>Hello World!</Fragment>
+)
+
+render(
+<Index />,
+  document.getElementById('root')
+)
